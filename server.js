5dc43fcaa846c6e1615b51e446166f53415eefdb
surajbahugunalc <surajb@linecraft.ai>
2024-09-11 00:35:08 +0530

    remove ui from backend

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://kishmish-ui.vercel.app/",
     methods: ["GET", "POST"]
   }
 });
