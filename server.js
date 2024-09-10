69b76cc60baed2635178051d8b4e2583a1d7849e
surajbahugunalc <surajb@linecraft.ai>
2024-09-11 00:43:16 +0530

    remove ui from backend

--- server.js
+++ server.js
@@ -126,5 +126,6 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
