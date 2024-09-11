983d692f02bfca01fc2b380786908f3194b8484d
surajbahugunalc <surajb@linecraft.ai>
2024-09-11 12:06:48 +0530

    adb

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://43.204.130.30",
+    origin: "http://43.204.130.30:3000",
     // origin: "https://kishmish-ui.vercel.app",
     methods: ["GET", "POST"]
   }
